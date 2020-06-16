090b1abb901375f96eb721e032d1d757cadc8a1b
Yujie wU <ywu3-6>
2020-06-17 03:24:36 +1000

    adding new project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
+import { CardList } from './components/card-list/CardList';
+import SearchBox from './components/search-box/SearchBox';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: '',
+    };
+  }
+
+  async componentDidMount() {
+    const res = await axios.get(`https://jsonplaceholder.typicode.com/users`);
+    this.setState({ monsters: res.data });
+  }
+
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+    // filter the array if any element contains the search key word, then return to a new array.
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="Search Monsters..."
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/card/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Card.css';
+
+const Card = (props) => (
+  <div className="card-container">
+    <img
+      alt="monster"
+      src={`https://robohash.org/${props.monster.id}?set=set4&size=180x180`}
+    />
+    <h2> {props.monster.name} </h2>
+    <p> {props.monster.email} </p>
+  </div>
+);
+
+export default Card;
