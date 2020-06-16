c0434fd8285dc9c994a3c372469a391a52b58468
Yujie wU <ywu3-6>
2020-06-17 03:28:34 +1000

    Updates

---
+++ precache-manifest.0a7c4ee1bec74ee1e570bef13a8142e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6e4013efeea3b72c75e49515675b930",
+    "url": "/react-exercise-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1586e14d155252e89335",
+    "url": "/react-exercise-monsters-rolodex/static/css/main.aece2b02.chunk.css"
+  },
+  {
+    "revision": "d1132e0162ea641e2d46",
+    "url": "/react-exercise-monsters-rolodex/static/js/2.02a0632e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-exercise-monsters-rolodex/static/js/2.02a0632e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1586e14d155252e89335",
+    "url": "/react-exercise-monsters-rolodex/static/js/main.9fde2279.chunk.js"
+  },
+  {
+    "revision": "56d95afc8143e989ea1c",
+    "url": "/react-exercise-monsters-rolodex/static/js/runtime-main.5af6e9fa.js"
+  }
+]);
